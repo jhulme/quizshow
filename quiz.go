2535454765c1390cedda7dafcfb44c3bc6f34715
John Hulme <john.hulme@rakuten.com>
2018-08-24 17:06:52 +0100

    Added functionality to record answers and output final score.

--- quiz.go
+++ quiz.go
@@ -13,10 +13,13 @@ import (
 )
 
 func main() {
-	loadProblems("files/problems.csv")
+	sheet := loadProblems("files/problems.csv")
+	answ, quest := calcScore(sheet)
+	fmt.Printf("You scored: %d/%d\n", answ, quest)
 }
 
-func loadProblems(filepath string) {
+func loadProblems(filepath string) (results []bool) {
+	var scoresheet []bool
 	data, err := ioutil.ReadFile(filepath)
 	checkErr(err)
 
@@ -33,22 +36,36 @@ func loadProblems(filepath string) {
 			log.Fatal(err)
 		}
 
-		//fmt.Println(record[0])
-		printQuestion(record[0], record[1])
+		scoresheet = append(scoresheet, printQuestion(record[0], record[1]))
 	}
-
+	return scoresheet
 }
 
-func printQuestion(question, answer string) {
+func printQuestion(question, answer string) (result bool) {
 	fmt.Printf("What is %s?\n", question)
 	input := bufio.NewReader(os.Stdin)
 	text, _ := input.ReadString('\n')
 
 	if strings.TrimRight(text, "\n") == answer {
-		fmt.Print("Correct Answer!")
+		fmt.Print("Correct Answer!\n")
+		return true
 	} else {
 		fmt.Printf("Incorrect Answer! Expecting %s\n", answer)
+		return false
 	}
+}
+
+func calcScore(scores []bool) (score, questions int) {
+	//use array to log either correct or incorrect - count number of corrects at end
+	var correct []bool
+	questions = len(scores)
+	for _, v := range scores {
+		if v == true {
+			correct = append(correct, v)
+		}
+	}
+	score = len(correct)
+	return score, questions
 }
 
 func checkErr(e error) {
